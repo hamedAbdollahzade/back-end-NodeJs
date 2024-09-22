6c00681129060e519aacf6a9c0142aace178d0cf
Hamed abdollahzade <hamedabdollahzade.ab@gmail.com>
2024-09-22 13:58:07 +0330

    module http

--- app.js
+++ app.js
@@ -19,12 +19,39 @@ const fs = require("node:fs");
 // });
 // console.log(files);
 // ------------------------------------------
-const EventEmitter = require("node:events");
-const emitter = new EventEmitter();
-// اول این نوشته میشه
-emitter.on("MessageLogged", (data) => {
-  console.log("Listener called", data);
-});
-// بعد اینجا زنگوله به صدا در میاد
-emitter.emit("MessageLogged", { id: 1, name: "Hamed-AB" });
+// const EventEmitter = require("node:events");
+// const emitter = new EventEmitter();
+// // اول این نوشته میشه
+// emitter.on("MessageLogged", (data) => {
+//   console.log("Listener called", data);
+// });
+// // بعد اینجا زنگوله به صدا در میاد
+// emitter.emit("MessageLogged", { id: 1, name: "Hamed-AB" });
 // ------------------------------------------
+const http = require("node:http");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.write("Hello from Home Page ");
+    res.end();
+  }
+  if (req.url === "/api/course") {
+    res.write(
+      JSON.stringify([
+        { id: 1, name: "Node.js" },
+        { id: 2, name: "React.js" },
+        { id: 3, name: "Angular.js" },
+      ])
+    );
+    res.end();
+  }
+});
+
+/// ! روش قدیمی
+// server.on("connection", (socket) => {
+//   console.log("New Connection is Connected");
+// });
+
+server.listen(3000);
+console.log("server is Listening in port 3000");
+// -----------------------------------------------
