50d364cbb0f00dbadfe74fba86d3a98d3a4fb2be
Hamed abdollahzade <hamedabdollahzade.ab@gmail.com>
2024-09-22 13:07:08 +0330

    const fs = require("node:fs")

--- app.js
+++ app.js
@@ -2,8 +2,12 @@
 // const data = path.parse(__filename);
 // console.log(data);
 // -----------------------------------------
-const os = require("node:os");
-const total = os.totalmem();
-const free = os.freemem();
-console.log("free ===>", free, "Total ===>", total);
+// const os = require("node:os");
+// const total = os.totalmem();
+// const free = os.freemem();
+// console.log("free ===>", free, "Total ===>", total);
 // ------------------------------------
+const fs = require("node:fs");
+const files = fs.readdirSync("./");
+console.log(files);
+// ------------------------------------------
