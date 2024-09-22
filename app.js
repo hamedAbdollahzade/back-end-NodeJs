cd549e37ea04e55b81b05a64367df4bf3b6669f9
Hamed abdollahzade <hamedabdollahzade.ab@gmail.com>
2024-09-22 13:00:57 +0330

    node:os

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
-const path = require("node:path");
-
-const data = path.parse(__filename);
-console.log(data);
+// const path = require("node:path");
+// const data = path.parse(__filename);
+// console.log(data);
+// -----------------------------------------
+const os = require("node:os");
+const total = os.totalmem();
+const free = os.freemem();
+console.log("free ===>", free, "Total ===>", total);
+// ------------------------------------
