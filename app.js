53029a4249d6ef19ace8cb22349c33c0bc498a79
Hamed abdollahzade <hamedabdollahzade.ab@gmail.com>
2024-09-24 15:04:21 +0330

    add package 'dotEnv'

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require("express");
-
 const app = express();
+// برای اینکه بتونیم فایل های ای ان وی رو بخونیم باید از این پکیج استفاده کنیم
+const dotEnv = require("dotenv");
+dotEnv.config();
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+const port = process.env.APP_PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
-
-
