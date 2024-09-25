80aecb88eb71d8213c40b9d203f2178b047c9707
Hamed abdollahzade <hamedabdollahzade.ab@gmail.com>
2024-09-25 13:19:40 +0330

    POST || PUT || DELETE

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const app = express();
+
+// اگر این میدل‌ور را استفاده نکنید و تلاش کنید به req.body دسترسی پیدا کنید، معمولاً undefined دریافت خواهید کرد، زیرا Express نمی‌تواند بدنه درخواست را به درستی تجزیه کند.
+app.use(express.json());
+
 // برای اینکه بتونیم فایل های ای ان وی رو بخونیم باید از این پکیج استفاده کنیم
 const dotEnv = require("dotenv");
 dotEnv.config();
@@ -14,6 +18,7 @@ const courses = [
   { id: 2, name: "React" },
   { id: 3, name: "Angular" },
 ];
+console.log(courses);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
@@ -27,5 +32,33 @@ app.get("/api/courses", (req, res) => {
 app.get("/api/courses/:id/:name", (req, res) => {
   const course = courses.find((c) => c.id === parseInt(req.params.id));
   if (!course) return res.status(404).send("Course not found");
-  res.json([course, req.params.name , req.query.sort]);
+  // "/api/courses/:id/:name?sort=id" ==> bade ? mishe query hamun !
+  res.json([course, req.params.name, req.query.sort]);
+});
+
+app.post("/api/courses", (req, res) => {
+  const { name } = req.body;
+  if (!name || name.length < 3) return res.status(400).send("Name is required");
+  const newCourse = {
+    id: courses.length + 1,
+    name: name,
+  };
+  courses.push(newCourse);
+  res.send(courses);
+});
+
+app.put("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) return res.status(404).send("Course not found");
+  const { name } = req.body;
+  if (!name || name.length < 3) return res.status(400).send("Name is required");
+  course.name = name;
+  res.send(courses);
+});
+
+app.delete("/api/courses/:id", (req, res) => {
+  const index = courses.findIndex((c) => c.id === parseInt(req.params.id));
+  if (index === -1) return res.status(404).send("Course not found");
+  courses.splice(index, 1);
+  res.send(courses);
 });
