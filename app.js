234c98cb5b4957877964ddd6dd03a6fe5084b7d2
Hamed abdollahzade <hamedabdollahzade.ab@gmail.com>
2024-09-25 11:32:07 +0330

    params and query

--- app.js
+++ app.js
@@ -9,6 +9,23 @@ app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
+const courses = [
+  { id: 1, name: "Node.js" },
+  { id: 2, name: "React" },
+  { id: 3, name: "Angular" },
+];
+
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+
+app.get("/api/courses", (req, res) => {
+  res.json(courses);
+});
+
+// علامت ؟ ینی اگه پارامتر وجود داشت بفرست اگه وجود نداشت هم خطا نده
+app.get("/api/courses/:id/:name", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) return res.status(404).send("Course not found");
+  res.json([course, req.params.name , req.query.sort]);
 });
